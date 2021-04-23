178f94a2056ac67cd7b0f2a7154153e671cef193
Chetan Kumar <79582069+chetanfirstfloor@users.noreply.github.com>
2021-04-23 19:10:54 +0530

    documents api updated

--- models/driver/driver_documents.js
+++ models/driver/driver_documents.js
@@ -10,34 +10,24 @@ module.exports = (sequelize, Sequelize) => {
         },
         driver_id:{
             type: Sequelize.INTEGER,
-            allowNull: false,
         },
         dl_id:{
             type: Sequelize.INTEGER,
-            allowNull: false,
         },
         insurance_id:{
             type: Sequelize.INTEGER,
-            allowNull: false,
         },
         address_proof_id:{
             type: Sequelize.INTEGER,
-            allowNull: false,
         },
         profile_pic_id:{ 
             type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        status_id:{
-            type:Sequelize.INTEGER,
         },
         vehicle_pic_id:{
             type:Sequelize.INTEGER,
-            allowNull:false,
         },
         vahicle_rc_id:{
             type:Sequelize.INTEGER,
-            allowNull:false,
         },
         is_deleted: {
             type: Sequelize.INTEGER,
